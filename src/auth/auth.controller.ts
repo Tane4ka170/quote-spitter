1ed95d6d1a5f0f3d43b57fca066a92ef80f55421
Tane4ka170 <tane4ka170@gmail.com>
2024-07-25 00:37:08 +0300

    upd

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,11 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
+import { Request as ExpressRequest } from 'express';
+
+interface UserRequest extends ExpressRequest {
+  user?: any;
+}
 
 @Controller('auth')
 export class AuthController {
@@ -8,7 +13,7 @@ export class AuthController {
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
+  async login(@Request() req: UserRequest) {
     return this.authService.login(req.user);
   }
 }
