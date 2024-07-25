bd6aca5ea6398dc606a08f31fab287b2cdd1baa1
Tane4ka170 <tane4ka170@gmail.com>
2024-07-25 22:58:18 +0300

    update main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://bsp-mini-chat-backend.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(3000);
 }
 bootstrap();
