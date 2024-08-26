a38e0b4361e524a751dd0d2f2a52956d90e1f396
Tane4ka170 <tane4ka170@gmail.com>
2024-08-27 01:01:07 +0300

    Replace logo with RandomQuote component in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import RandomQuote from "./components/RandomQuote/RandomQuote";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RandomQuote />
+    </div>
+  );
 }
 
 export default App;
